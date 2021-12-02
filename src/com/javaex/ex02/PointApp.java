cf7031451ba5629153e6206625afffedb34c33d9
Hojun_Yoo <lisiee0@naver.com>
2021-12-02 17:56:34 +0900

    12-02

--- src/com/javaex/ex02/PointApp.java
+++ src/com/javaex/ex02/PointApp.java
@@ -8,6 +8,8 @@ public class PointApp {
 		Point p01= new Point(2,3);
 		Point p02= new Point(5,9);
 		
+		Point p03= p02;
+		
 		System.out.println("#getClass()");
 		System.out.println(p00.getClass());
 		System.out.println(p01.getClass());
@@ -26,6 +28,12 @@ public class PointApp {
 		System.out.println(p02.toString());
 		System.out.println("====================================");
 		
+		System.out.println("#equals()");
+		System.out.println(p00.equals(p02));
+		System.out.println(p00.equals(p01));
+		System.out.println(p00==p01);
+		System.out.println(p02==p03);
+		System.out.println("====================================");
 	}
 
 }
---
+++ src/com/javaex/ex03/PointApp.java
@@ -0,0 +1,23 @@
+package com.javaex.ex03;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		
+		Point a= new Point(2, 3);
+		Point b= new Point(2, 3);
+		Point c= new Point(100, 51);
+		
+		Point d= c;
+		// 주소 비교
+		System.out.println(a==b);
+		System.out.println(c==d);
+		System.out.println("================================");
+		
+		System.out.println(a.equals(b));
+		
+		
+		
+	}
+
+}
