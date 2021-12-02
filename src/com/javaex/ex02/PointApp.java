95b4029b48853ede5b66ddc97a848e945c20c7c1
Hojun_Yoo <lisiee0@naver.com>
2021-12-02 16:23:38 +0900

    chapter03

---
+++ src/com/javaex/ex02/PointApp.java
@@ -0,0 +1,25 @@
+package com.javaex.ex02;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+
+		Point p00= new Point(2,3);
+		Point p01= new Point(2,3);
+		Point p02= new Point(5,9);
+		
+		System.out.println("#getClass()");
+		System.out.println(p00.getClass());
+		System.out.println(p01.getClass());
+		System.out.println(p02.getClass());
+		System.out.println("====================================");
+		
+		System.out.println("#hashCode()");
+		System.out.println(p00.hashCode());
+		System.out.println(p01.hashCode());
+		System.out.println(p02.hashCode());
+		System.out.println("====================================");
+
+	}
+
+}
