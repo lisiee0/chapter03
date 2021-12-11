b9d505ac04e2e86359ca883214b222cb64e14ba1
Hojun_Yoo <lisiee0@naver.com>
2021-12-12 00:37:10 +0900

    ex29

--- src/com/javaex/ex06/StringApp02.java
+++ src/com/javaex/ex06/StringApp02.java
@@ -3,7 +3,7 @@ package com.javaex.ex06;
 public class StringApp02 {
 
 	public static void main(String[] args) {
-		
+		/*
 		String a= "abcd";
 		String b= ",efg";
 		
@@ -29,14 +29,18 @@ public class StringApp02 {
 		for(int i= 0; i<sArray.length; i++) {
 			System.out.println(sArray[i]);
 		}
-		
+		*/
 		String str= "Hello java!";
 		
-		String r01= str.substring(3);
-		System.out.println(r01);
+			
+		String r01= str.substring(0);
+		System.out.println(str.length());
 		
+		
+		/*
 		String r02= str.substring(3, 8); // 3부터 8전까지
 		System.out.println(r02);
+		*/
 	}
 
 }
